4bbca7dda8d0e0fb3bb083b8db3967ab0a1faa3f
kdh0324 <kdh0324@postech.ac.kr>
2022-04-25 17:42:41 +0900

    Implement step2

--- hw6/eval.ml
+++ hw6/eval.ml
@@ -8,25 +8,60 @@ type stoval =
   | Delayed of exp * env
 
  and stack =
-   Hole_SK
+     Hole_SK
    | Frame_SK of stack * frame
 
  and state =
-   Anal_ST of (stoval Heap.heap) * stack * exp * env
+     Anal_ST of (stoval Heap.heap) * stack * exp * env
    | Return_ST of (stoval Heap.heap) * stack * value 
 
  (* Define your own datatypes *)
- and env = NOT_IMPLEMENT_ENV
- and value = NOT_IMPLEMENT_VALUE
- and frame = NOT_IMPLEMENT_FRAME
+ (* n -> l *)
+ and env = int -> Heap.loc option
+
+ and value = 
+		Lam_V of exp * env
+	| Pair_V of exp * exp * env
+	| Eunit_V
+	| Inl_V of exp * env
+	| Inr_V of exp * env
+	| True_V
+	| False_V
+	| Num_V of int
+	| Plus_V
+	| Minus_V
+	| Eq_V
+	
+ and frame = 
+		App_F of exp * env
+	| Fst_F
+	| Snd_F
+	| Case_F of exp * exp * env
+	| Ifthenelse_F of exp * exp * env
+	| Plus_F
+	| Plus_Fst_F of exp * env
+	| Plus_Snd_F of value
+	| Minus_F
+	| Minus_Fst_F of exp * env
+	| Minus_Snd_F of value
+	| Eq_F
+	| Eq_Fst_F of exp * env
+	| Eq_Snd_F of value
+	| L_F of Heap.loc
 
 (* Define your own empty environment *)
-let emptyEnv = NOT_IMPLEMENT_ENV
+let emptyEnv = fun x -> None
 
 (* Implement the function value2exp : value -> Tml.exp
  * Warning : If you give wrong implementation of this function,
  *           you wiil receive no credit for the entire third part!  *)
-let value2exp _ = raise NotImplemented
+let value2exp v = 
+	match v with
+	  Eunit_V -> Eunit
+	| True_V -> True
+	| False_V -> False
+	| Num_V n -> Num n
+	| _ -> raise NotConvertible
 
 (* Problem 1. 
  * texp2exp : Tml.texp -> Tml.exp *)
@@ -128,9 +163,7 @@ let rec step1 e =
 		in
   match e with
     App (Plus, Pair (Num n1, Num n2)) -> Num(n1 + n2)
-  | App (Minus, Pair (Num n1, Num n2)) -> 
-    if n1 >= n2 then Num(n1 - n2)
-    else Num(0)
+  | App (Minus, Pair (Num n1, Num n2)) -> Num(n1 - n2)
   | App (Eq, Pair (Num n1, Num n2)) -> 
     if n1 = n2 then True
     else False
@@ -154,7 +187,72 @@ let rec step1 e =
 
 (* Problem 3. 
  * step2 : state -> state *)
-let step2 _ = raise NotImplemented
+let step2 st = 
+	let newEnv env l = fun x -> if x = 0 then Some l else env (x - 1) in
+	match st with
+	  Anal_ST (h, sk, e, env) ->
+		(match e with
+			Ind n -> 
+			let l = env n in
+			(match l with
+				None -> raise Stuck
+			| Some l' ->
+				(match Heap.deref h l' with
+					Computed v -> Return_ST(h, sk, v)
+				| Delayed (e', env') -> Anal_ST(h, Frame_SK(sk, L_F(l')), e', env')))
+		| Lam e' -> Return_ST(h, sk, Lam_V(e', env))
+		| App (e1, e2) -> Anal_ST(h, Frame_SK(sk, App_F(e2, env)), e1, env)
+		| Pair (e1, e2) -> Return_ST(h, sk, Pair_V(e1, e2, env))
+		| Fst e' -> Anal_ST(h, Frame_SK(sk, Fst_F), e', env)
+		| Snd e' -> Anal_ST(h, Frame_SK(sk, Snd_F), e', env)
+		| Eunit -> Return_ST(h, sk, Eunit_V)
+		| Inl e' -> Return_ST(h, sk, Inl_V(e', env))
+		| Inr e' -> Return_ST(h, sk, Inr_V(e', env))
+		| Case (e', e1, e2) -> Anal_ST(h, Frame_SK(sk, Case_F(e1, e2, env)), e', env)
+		| Fix e' ->
+			let h', l = Heap.allocate h (Delayed (Fix e', env)) in
+			Anal_ST(h', sk, e', newEnv env l)
+		| True -> Return_ST(h, sk, True_V)
+		| False -> Return_ST(h, sk, False_V)
+		| Ifthenelse (e', e1, e2) -> Anal_ST(h, Frame_SK(sk, Ifthenelse_F(e1, e2, env)), e', env)
+		| Num n -> Return_ST(h, sk, Num_V n)
+		| Plus -> Return_ST(h, sk, Plus_V)
+		| Minus -> Return_ST(h, sk, Minus_V)
+		| Eq -> Return_ST(h, sk, Eq_V))
+	| Return_ST (h, sk, v) ->
+		(match sk with
+			Hole_SK -> raise Stuck
+		| Frame_SK (sk, f) -> 
+			match f, v with
+			  App_F (e, env), Plus_V -> Anal_ST(h, Frame_SK(sk, Plus_F), e, env)
+			|	App_F (e, env), Minus_V -> Anal_ST(h, Frame_SK(sk, Minus_F), e, env)
+			|	App_F (e, env), Eq_V -> Anal_ST(h, Frame_SK(sk, Eq_F), e, env)
+			| App_F (e, env), Lam_V (e', env') -> 
+				let h', l = Heap.allocate h (Delayed(e, env)) in
+				Anal_ST(h', sk, e', newEnv env' l)
+			| Fst_F, Pair_V (e1, e2, env) -> Anal_ST(h, sk, e1, env)
+			| Snd_F, Pair_V (e1, e2, env) -> Anal_ST(h, sk, e2, env)
+			| Case_F (e1, e2, env), Inl_V (e, env') ->
+				let h', l = Heap.allocate h (Delayed(e, env')) in
+				Anal_ST(h', sk, e1, newEnv env l)
+			| Case_F (e1, e2, env), Inr_V (e, env') ->
+				let h', l = Heap.allocate h (Delayed(e, env')) in
+				Anal_ST(h', sk, e2, newEnv env l)
+			| Ifthenelse_F (e1, e2, env), True_V -> Anal_ST(h, sk, e1, env)
+			| Ifthenelse_F (e1, e2, env), False_V -> Anal_ST(h, sk, e2, env)
+			| Plus_F, Pair_V(e1, e2, env) -> Anal_ST(h, Frame_SK(sk, Plus_Fst_F(e2, env)), e1, env)
+			| Plus_Fst_F (e, env), Num_V n -> Anal_ST(h, Frame_SK(sk, Plus_Snd_F v), e, env)
+			| Plus_Snd_F (Num_V n1), Num_V n2 -> Return_ST(h, sk, Num_V(n1 + n2))
+			| Minus_F, Pair_V(e1, e2, env) -> Anal_ST(h, Frame_SK(sk, Minus_Fst_F(e2, env)), e1, env)
+			| Minus_Fst_F (e, env), Num_V n -> Anal_ST(h, Frame_SK(sk, Minus_Snd_F v), e, env)
+			| Minus_Snd_F (Num_V n1), Num_V n2 -> Return_ST(h, sk, Num_V(n1 - n2))
+			| Eq_F, Pair_V(e1, e2, env) -> Anal_ST(h, Frame_SK(sk, Eq_Fst_F(e2, env)), e1, env)
+			| Eq_Fst_F (e, env), Num_V n -> Anal_ST(h, Frame_SK(sk, Eq_Snd_F v), e, env)
+			| Eq_Snd_F (Num_V n1), Num_V n2 -> 
+				let bv = if n1 = n2 then True_V else False_V in
+				Return_ST(h, sk, bv)
+			| L_F l, _ -> Return_ST(Heap.update h l (Computed v), sk, v)
+			| _ -> raise Stuck)
                     
 (* exp2string : Tml.exp -> string *)
 let rec exp2string exp = 
